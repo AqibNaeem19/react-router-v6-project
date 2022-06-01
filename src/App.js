f74216c843b37c8363b2767a4c38fc0df8ac7b02
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-06-01 17:54:42 +0500

    Added different routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+import ErrorPage from "./Pages/ErrorPage";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to react router v6</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/about' element={ < About />} />
+        <Route path='/profile' element={ <Profile />} />
+        <Route path='*' element={ <ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
